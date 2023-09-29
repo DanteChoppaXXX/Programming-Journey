35c8c2302e5a0309c1e33237e651a6be04d4274a
DanteChoppaXXX <only1dantechoppa@gmail.com>
2023-09-29 10:04:12 -0400

    Update

--- PYTHON/Automate_the_boring_stuff/2_Flow_Control/code0.py
+++ PYTHON/Automate_the_boring_stuff/2_Flow_Control/code0.py
@@ -2,7 +2,10 @@
 import time
 
 name = input('what is your name? : ')
-password = input('password : ')
+age = int(input('How old are you? : '))
+# password = input('password : ')
+
+# IF AND ELSE STATEMENT
 
 # if name == 'Kiriko':
 #     print('Hello, Kiriko')
@@ -15,13 +18,26 @@ password = input('password : ')
 #     print('Intruder! FUCK OFF!!!')
 
 # A more programmatic way to write the above code
-if name == 'Kiriko':
-    if name == 'Kiriko' and password == 'rogueninja':
-        time.sleep(0.5)
-        print('ACCESS GRANTED!')
-        time.sleep(1)
-        print('WELCOME KIRIKO!')
-    else:
-        print('Wrong Password')
+# if name == 'Kiriko':
+#     if name == 'Kiriko' and password == 'rogueninja':
+#         time.sleep(0.5)
+#         print('ACCESS GRANTED!')
+#         time.sleep(1)
+#         print('WELCOME KIRIKO!')
+#     else:
+#         print('Wrong Password')
+# else:
+#     print('Intruder! FUCK OFF!!!')
+
+# ELIF STATEMENT
+if name == 'Alice':
+    print('Hi, Alice')
+elif age < 12:
+    print('You are Not Alice, kiddo')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not Alice, grannie.')
 else:
-    print('Intruder! FUCK OFF!!!')
+    print('You are neither Alice nor a little kid.')
+
--- PYTHON/Automate_the_boring_stuff/2_Flow_Control/code1.py
+++ PYTHON/Automate_the_boring_stuff/2_Flow_Control/code1.py
@@ -1,15 +1,50 @@
 #!../bin/python3
 import time
+import random
 
-name = input('What is your name? : ')
-clan = input('What clan do you belong? :')
+# name = input('What is your name? : ')
+# clan = input('What clan do you belong? :')
 
-if name == 'Sasori':
-    print('WELCOME, GREAT PUPPET MASTER!')
-elif clan != 'Akatsuki':
-    print('You Do not Belong Here!')
-elif clan == 'Uchiha':
-    print('Kill Him!!!')
-else:
-    print('Leave Now, Shinobis Only!')
+# while name == 'Sasori':
+#     print('WELCOME, GREAT PUPPET MASTER!')
+
+# digit = 3
+
+# while digit < 5:
+#     print('Hack The World!!!')
+#     digit = digit + 1
+    
+# An Annoying Loop
+# name = ''
+# while name != 'your name':
+#     print('Please type your name.')
+#     name = input()
+# print('Thank You!')
+
+# coin_side0 = 'Head'
+# coin_side1 = 'Tail'
+print('===== WELCOME =====')
+time.sleep(1)
+input('Press Enter To Continue')
+time.sleep(0.5)
+print('=========> TOSS DA COIN <==========')
+print("Loading.", end="")
+time.sleep(1)
+print(".", end="") 
+time.sleep(1) 
+print(".")
+
+time.sleep(3)
+while True:
+#Simulate a coin toss where (0 is for Head and 1 is for Tail)
+    toss = random.randint(0,1)
+    choice = input('Head or Tail? : ')
+    choice = choice.lower()
+    if choice == 'head' or choice == 'tail':
+        time.sleep(3)
+        print(f"Result:{'Head' if toss == 0 else 'Tail'}")
+        break
+    else:
+        print('Try again!')
+            
 
