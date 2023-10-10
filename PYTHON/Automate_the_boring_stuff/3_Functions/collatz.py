8ab56fdf1e7d0e7f5840c64190f529c0ae720fa7
DanteChoppaXXX <only1dantechoppa@gmail.com>
2023-10-10 06:29:11 -0400

    End of chapter 3 of Automate the boring stuff

--- PYTHON/Automate_the_boring_stuff/3_Functions/collatz.py
+++ PYTHON/Automate_the_boring_stuff/3_Functions/collatz.py
@@ -2,13 +2,20 @@
 
 def collatz(number):
     if number % 2 == 0:
-        print(number // 2)
-        
+        result = number // 2
+        print(result)
+        return result
     if number % 2 == 1:
-        print(3 * number + 1)
+        result = 3 * number + 1
+        print(result)
+        return result
 
 try:
-    collatz(int(input('Enter Number: ')))
+    print('Enter Number: ')
+    number = int(input())
+    
+    while number != 1:
+        number = collatz(number)
 
 except ValueError:
         print('You must enter an integer')
