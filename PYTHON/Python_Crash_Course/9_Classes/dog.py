349a87e40edee7830a0c7e70a7edf619453cd238
DanteChoppaXXX <only1dantechoppa@gmail.com>
2023-10-27 01:24:32 -0400

    classes

--- PYTHON/Python_Crash_Course/9_Classes/dog.py
+++ PYTHON/Python_Crash_Course/9_Classes/dog.py
@@ -10,8 +10,19 @@ class Dog():
 
     def sit(self):
         """Simulate a dog sitting in response to a command."""
-        print(f"{self.name.title()}, is now sitting")
+        print(f"{self.name.title()} is now sitting")
 
     def roll_over(self):
         """Simulate rolling over in response to a command."""
-        print(f"{self.name.title()}, rolled over!")
+        print(f"{self.name.title()} rolled over!")
+
+name = input("What's your dog's name? ")
+age = int(input("How old is your dog? "))
+
+my_dog = Dog(name, age)
+
+print(f"My dog's name is {my_dog.name.title()}.")
+print(f"{my_dog.name} is {my_dog.age} years old.\n")
+
+my_dog.sit()
+my_dog.roll_over()
