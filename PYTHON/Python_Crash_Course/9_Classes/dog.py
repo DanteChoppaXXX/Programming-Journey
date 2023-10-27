0363654f76a3a27b949df7785da446a723ae8457
DanteChoppaXXX <only1dantechoppa@gmail.com>
2023-10-27 01:43:54 -0400

    classes

--- PYTHON/Python_Crash_Course/9_Classes/dog.py
+++ PYTHON/Python_Crash_Course/9_Classes/dog.py
@@ -16,13 +16,19 @@ class Dog():
         """Simulate rolling over in response to a command."""
         print(f"{self.name.title()} rolled over!")
 
-name = input("What's your dog's name? ")
-age = int(input("How old is your dog? "))
+keep_going = True
+while keep_going:
+    name = input("What's your dog's name? ")
+    age = int(input("How old is your dog? "))
 
-my_dog = Dog(name, age)
+    my_dog = Dog(name, age)
 
-print(f"My dog's name is {my_dog.name.title()}.")
-print(f"{my_dog.name} is {my_dog.age} years old.\n")
+    print(f"My dog's name is {my_dog.name.title()}.")
+    print(f"{my_dog.name} is {my_dog.age} years old.\n")
 
-my_dog.sit()
-my_dog.roll_over()
+    my_dog.sit()
+    my_dog.roll_over()
+
+    moreDogs = input("Do you have another dog? (y or n) :")
+    if moreDogs == "n":
+        keep_going = False
