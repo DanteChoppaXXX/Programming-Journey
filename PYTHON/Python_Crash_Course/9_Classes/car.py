6a0f53c32bced76ccfac2a3c69e3ed9ada90e92a
DanteChoppaXXX <only1dantechoppa@gmail.com>
2023-10-30 02:42:02 -0400

    classes

--- PYTHON/Python_Crash_Course/9_Classes/car.py
+++ PYTHON/Python_Crash_Course/9_Classes/car.py
@@ -39,17 +39,34 @@ class Car():
         for i in range(3):
             print("Running low on gas\nFill up!")
 
+class Battery():
+    """A simple attempt to model a battery for an electric car."""
+    def __init__(self, battery_size=70):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+    def describe_battery(self):
+        """Print a statement describing the battery."""
+        print(f"This car has a {self.battery_size}-kwh battery")
+
+    def get_range(self):
+        """Print a  statement about the range this battery provides."""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+    
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles"""
     def __init__(self, make, model, year):
         """Initialize attributes of the parent class.
         Then initialize attributes specific to an electric car."""
         super().__init__(make, model, year)
-        self.battery_size = 70
+        self.battery = Battery()
 
-    def describe_battery(self):
-        """Print a statement describing the battery."""
-        print(f"This car has a {self.battery_size}-kwh battery")
 
     def fill_gas_tank(self):
         """Elecric cars don't have gas tanks."""
@@ -65,5 +82,6 @@ my_new_car.fill_gas_tank()
 
 my_tesla = ElectricCar('tesla', 'model s', 2023)
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
-my_tesla.fill_gas_tank()
+my_tesla.battery.describe_battery()
+#my_tesla.fill_gas_tank()
+my_tesla.battery.get_range()
--- PYTHON/Python_Crash_Course/9_Classes/restaurant.py
+++ PYTHON/Python_Crash_Course/9_Classes/restaurant.py
@@ -22,22 +22,38 @@ class Restaurant():
         self.number_served = served
         print(f"Total Customer Served: {self.number_served}")
 
-restaurant = Restaurant("Oasis", "Eatery")
-restaurant0 = Restaurant("Sandton Gold", "Hotel")
-restaurant1 = Restaurant("Dalco", "Guest House")
+class IceCreamStand(Restaurant):
+    """Modeling a child class for the Restaurant class."""
+    def __init__(self, restaurant_name, restaurant_type, flavors):
+        super().__init__(restaurant_name, restaurant_type)
+        """Create a list of ice cream flavors"""
+        flavors = ['vanilla', 'strawberry', 'banana', 'chocolate']
+        self.flavors = flavors
 
-#print(f"{restaurant.name.title()}")
-#print(f"{restaurant.type}")
+    def flavors_list(self):
+        """Display the list of available flavors to the customer."""
+        print(self.flavors)
 
-restaurant.describe_restaurant()
-restaurant.set_number_served(800)
-restaurant.open_restaurant()
-print('___________________')
-restaurant0.describe_restaurant()
-restaurant0.set_number_served(1500)
-print('___________________')
-restaurant1.describe_restaurant()
-restaurant1.set_number_served(200)
-restaurant1.increment_number_served(2)
+iceCreamStand0 = IceCreamStand('Raheti', 'Eatery', 'flavors' )
+iceCreamStand0.flavors_list()
+
+
+# restaurant = Restaurant("Oasis", "Eatery")
+# restaurant0 = Restaurant("Sandton Gold", "Hotel")
+# restaurant1 = Restaurant("Dalco", "Guest House")
+
+# #print(f"{restaurant.name.title()}")
+# #print(f"{restaurant.type}")
+
+# restaurant.describe_restaurant()
+# restaurant.set_number_served(800)
+# restaurant.open_restaurant()
+# print('___________________')
+# restaurant0.describe_restaurant()
+# restaurant0.set_number_served(1500)
+# print('___________________')
+# restaurant1.describe_restaurant()
+# restaurant1.set_number_served(200)
+# restaurant1.increment_number_served(2)
 
 #print(f"Customer Served: {restaurant1.number_served}")
