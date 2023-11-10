66d0b024a4aa9867482a1448c11ad53233673567
DanteChoppaXXX <only1dantechoppa@gmail.com>
2023-11-10 04:39:45 -0500

    8_Functions

--- PYTHON/Python_Crash_Course/8_Functions/greeter.py
+++ PYTHON/Python_Crash_Course/8_Functions/greeter.py
@@ -1,17 +1,27 @@
 #!../bin/python3
 
-#Exercise 1
-def display_message():
-    """Tell everyone what you're learning."""
-    message = 'I am learning about functions in Python'
-    print(message)
+# #Exercise 1
+# def display_message():
+#     """Tell everyone what you're learning."""
+#     message = 'I am learning about functions in Python'
+#     print(message)
 
-display_message()
+# display_message()
 
-#Exercise 2
-def favorite_book(title):
-    """Tell what your favorite book is."""
-    message = f'One of my favorite books is {title}'
-    print(message)
+# #Exercise 2
+# def favorite_book(title):
+#     """Tell what your favorite book is."""
+#     message = f'One of my favorite books is {title}'
+#     print(message)
 
-favorite_book('Alice in Wonderland')
+# favorite_book('Alice in Wonderland')
+
+#PASSING A LIST TO A FUNCTION!!!
+def greet_users(names):
+    """Print a greeting to all users in the list."""
+    for i, name in enumerate(names, start=1):
+        msg = f'{i}. Hello, {name.title()}!'
+        print(msg)
+
+users = ['kate', 'john', 'chan', 'sasori']
+greet_users(users)
---
+++ PYTHON/Python_Crash_Course/8_Functions/printing_models.py
@@ -0,0 +1,23 @@
+#!../bin/python3
+
+#Start with some design that needs to be printed.
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+#Simulate printing each design, until none are left.
+#Move each design tto completed_models after printing.
+def print_models(unprinted_designs, completed_models):
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        #Simulate creating a 3D print from the design.
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+    #Display all completed models
+    print("\nThe following models have been printed:")
+    for i, completed_model in enumerate(completed_models, start=1):
+        print(f'{i}. {completed_model}')
+
+#print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
