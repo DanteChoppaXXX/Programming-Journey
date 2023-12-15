3c7a89f2c487697081897a418cf1aaff6fc9aaea
DanteChoppaXXX <only1dantechoppa@gmail.com>
2023-12-15 03:15:21 -0500

    Simple clock app

--- Small_Projects/clock.py
+++ Small_Projects/clock.py
@@ -8,23 +8,24 @@ def update():
     time_label.config(text=time_string)
 
     
-    day_string = strftime("%A")
+    day_string = strftime("======\n%A\n======")
     day_label.config(text=day_string)
 
     date_string = strftime("%B %d, %Y")
     date_label.config(text=date_string)
 
+    window.title("Choppa Clock")
     window.after(1000,update)
 
 window = Tk()
 
-time_label = Label(window, font=("Verdana",50), fg="#ffffff", bg="#000000")
+time_label = Label(window, font=("Times New Roman",50), fg="#ffffff", bg="#000000")
 time_label.pack()
 
 day_label = Label(window, font=("Cursive",25), fg="white", bg="#000000")
 day_label.pack()
 
-date_label = Label(window, font=("Papyrus", 25), fg="white", bg="#000000")
+date_label = Label(window, font=("Times New Roman", 25), fg="white", bg="#000000")
 date_label.pack()
 
 update()
