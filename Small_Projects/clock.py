c07dde23e4041d217504ed9efd2cb030b4d7e9af
DanteChoppaXXX <only1dantechoppa@gmail.com>
2023-12-15 03:03:08 -0500

    Simple clock app

---
+++ Small_Projects/clock.py
@@ -0,0 +1,32 @@
+#!./env/bin/python3
+from tkinter import *
+from time import *
+
+def update():
+    window.config(bg="#000000")
+    time_string = strftime("%I:%M:%S %p")
+    time_label.config(text=time_string)
+
+    
+    day_string = strftime("%A")
+    day_label.config(text=day_string)
+
+    date_string = strftime("%B %d, %Y")
+    date_label.config(text=date_string)
+
+    window.after(1000,update)
+
+window = Tk()
+
+time_label = Label(window, font=("Verdana",50), fg="#ffffff", bg="#000000")
+time_label.pack()
+
+day_label = Label(window, font=("Cursive",25), fg="white", bg="#000000")
+day_label.pack()
+
+date_label = Label(window, font=("Papyrus", 25), fg="white", bg="#000000")
+date_label.pack()
+
+update()
+
+window.mainloop()
